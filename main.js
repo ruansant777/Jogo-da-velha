1226f9afd0b6d85edb8c8dc8c6bac891ac082cee
ruansant777 <ruan.mendes.santana@escola.pr.gov.br>
2025-12-01 09:21:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+//variáveis
+let gameState = ['', '', '', '', '', '', '', '', '',];
+let activePlayer = 'X';
+let isGameRunning = true;
 
+//constante para condições de vitória
+const windConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  ];
+
+const statusElement = document.querySelector('status-display');
+const cellElements = document.querySelector('.cell');
+const restartBtn = document.querySelector('restart-button');
+
+//
